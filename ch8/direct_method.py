6f36de489f1ae91de1a666b0059b3c3486c09892
ChienMing Lin <jianming1481@gmail.com>
2023-11-05 16:19:38 +0800

    Working on Optical Flow Direct_Method

--- ch8/direct_method.py
+++ ch8/direct_method.py
@@ -1,5 +1,7 @@
 import cv2
 import random
+from jacobian_accumulator import JacobianAccumulator
+import time
 
 # Camera intrinsics
 fx = 718.856
@@ -8,6 +10,22 @@ cx = 607.1928
 cy = 185.2157
 # baseline
 baseline = 0.573
+
+# DirectPoseEstimationSingleLayer return Sophus::SE3d &T21)
+def DirectPoseEstimationSingleLayer(img1, img2, px_ref, depth_ref):
+    iterations = 10
+    cost = 0
+    lastCost = 0
+    t1 = time.time()
+    jaco_accu = JacobianAccumulator(img1, img2, px_ref, depth_ref)
+
+    for i in range(iterations):
+        jaco_accu.reset()
+        rng = slice(0, len(px_ref))
+        jaco_accu.accumulate_jacobian(rng)
+        H = jaco_accu.hessian()
+        b = jaco_accu.bias()
+
 
 def main():
     left_file = 'left.png'
@@ -33,6 +51,12 @@ def main():
         depth = fx*baseline/disparity
         depth_ref.append(depth)
         pixels_ref.append([x,y])
+
+    for i in range(1,6):
+        file_number = i
+        file_name = fmt_others.format(file_number)
+        img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+        # DirectPoseEstimationSingleLayer(left_img, img, pixels_ref, depth_ref, T_cur_ref);
 
 
 
---
+++ ch8/jacobian_accumulator.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+class JacobianAccumulator:
+    '''
+    input:
+        img1_: cv::Mat
+        img2_: cv::Mat
+        px_ref_: list[list[]]
+        depth_ref_: list[list[]]
+    return:
+        T21_: Sophus::SE3d
+    '''
+    def __init__(self, img1_, img2_, px_ref_, depth_ref_):
+        self.img1 = img1_
+        self.img2 = img2_
+        self.px_ref = px_ref_
+        self.depth_ref = depth_ref_
+        self.cost = 0.0
+
+    # Set Camera intrinsics
+    def set_camera_intrinsics(self, fx, fy, cx, cy, baseline):
+        self.fx = fx
+        self.fy = fy
+        self.cx = cx
+        self.cy = cy
+        self.base_line = baseline
+
+    # range = slice()
+    def accumulate_jacobian(self, rng):
+        # Parameter
+        half_patch_size = 1
+        hessian = np.zeros((6, 6))
+        ias = np.zeros(6)
+        tmp_cost = 0.0
+        print('accumulate_jacobian')
+
+        for i in range(rng.start, rng.stop):
+            # compute the projection in the second image
+            point_ref = self.depth_ref[i]*[(self.px_ref[i][0]-self.cx)/self.fx, (self.px_ref[i][1]-self.cy)/self.fy, 1]
+
+    def hessian(self):
+        self.H = np.zeros((6, 6))
+        return self.H
+
+    def bias(self):
+        self.b = np.zeros(6)
+        return self.b
+
+    def cost_func(self):
+        return self.cost
+
+    def projected_points(self):
+        # self.projection = [[0,0],[1,1],...]
+        return self.projection
+
+    def reset(self):
+        self.H = np.zeros((6, 6))
+        self.b = np.zeros(6)
+        self.cost = 0
