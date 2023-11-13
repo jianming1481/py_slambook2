7dc88fa157234febb690b21547e4ccf444db54cc
ChienMing Lin <jianming1481@gmail.com>
2023-11-13 23:33:55 +0800

    WIP

--- ch8/jacobian_accumulator.py
+++ ch8/jacobian_accumulator.py
@@ -20,6 +20,7 @@ class JacobianAccumulator:
         t_zero = np.zeros((3,))
         self.T21 = sp.SE3(R_identity.matrix(), t_zero)
         self.cost = 0.0
+        self.projection = [[0,0] for _ in range(len(self.px_ref))]
 
     # Set Camera intrinsics
     def set_camera_intrinsics(self, fx, fy, cx, cy, baseline):
@@ -36,6 +37,7 @@ class JacobianAccumulator:
         hessian = np.zeros((6, 6))
         ias = np.zeros(6)
         tmp_cost = 0.0
+        cnt_good = 0
         print('accumulate_jacobian')
 
         for i in range(rng.start, rng.stop):
@@ -43,7 +45,25 @@ class JacobianAccumulator:
             point_ref = list(map(lambda x: self.depth_ref[i] * x, [(self.px_ref[i][0]-self.cx)/self.fx,
                                                                    (self.px_ref[i][1]-self.cy)/self.fy,
                                                                    1]))
-            point_cur = self.T21 * point_ref;
+            point_cur = self.T21 * point_ref
+            if point_cur[2] < 0:        # depth invalid
+                continue
+
+            u = self.fx * point_cur[0] / point_cur[2] + self.cx
+            v = self.fy * point_cur[1] / point_cur[2] + self.cy
+            if u < half_patch_size or u > self.img2.shape[1] - half_patch_size or v < half_patch_size or v > self.img2.shape[0] - half_patch_size:
+                continue
+
+            self.projection[i] = [u, v]
+            X = point_cur[0]
+            Y = point_cur[1]
+            Z = point_cur[2]
+            Z2 = Z*Z
+            Z_inv = 1/Z
+            Z2_inv = Z_inv*Z_inv
+            cnt_good += 1
+
+            # and compute error and jacobian (11/13)
 
     def hessian(self):
         self.H = np.zeros((6, 6))
